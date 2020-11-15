cdbe8323ea4bb315ae521e8a8e67dd32364f8a12
timchanpon <62697984+timchanpon@users.noreply.github.com>
2020-11-15 11:31:42 +0900

    Update server.js

--- server.js
+++ server.js
@@ -5,34 +5,36 @@ const server = http.createServer();
 
 server.on('request', function(req, res)
 {
-	if (req.url === '/') {
+	const path = req.url;
+
+	if (path === '/') {
 		res.writeHead(302, { 'Location': '/index.html' });
 		res.end();
 
-		return console.log(`Request [302]: ${req.url}`);
-	} else if (req.url === '/favicon.ico') {
+		return console.log(`Request [302]: ${path}`);
+	} else if (path === '/favicon.ico') {
 		res.writeHead(200, { 'Content-Type': 'text/plain' });
 		res.end('Favicon is unavailable.');
 
-		return console.log(`Request [200]: ${req.url}`);
+		return console.log(`Request [200]: ${path}`);
 	}
 
-	const file = `dist${req.url}`;
-	const fileType = req.url.split('.').pop();
+	const file = `dist${path}`;
+	const fileType = path.split('.').pop();
 
 	fs.readFile(file, 'utf-8', (err, data) => {
 		if (!data) {
 			res.writeHead(404, { 'Content-Type': 'text/plain' });
 			res.end('Something went wrong. Please check the log.');
 
-			return console.log(`Request [404]: ${req.url}\n${err}`);
+			return console.log(`Request [404]: ${path}\n${err}`);
 		}
 
 		res.writeHead(200, { 'Content-Type': `text/${fileType}` });
 		res.write(data);
 		res.end();
 
-		console.log(`Request [200]: ${req.url}`);
+		console.log(`Request [200]: ${path}`);
 	});
 });
 
