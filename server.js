079dfca1fc0329ca9d5231b7f1e653a46ccc7795
timchanpon <62697984+timchanpon@users.noreply.github.com>
2021-01-18 19:10:08 +0900

    Convert tabs to spaces

--- server.js
+++ server.js
@@ -5,39 +5,39 @@ const server = http.createServer();
 
 server.on('request', function(req, res)
 {
-	const path = req.url;
+  const path = req.url;
 
-	if (path === '/') {
-		res.writeHead(302, { 'Location': '/index.html' });
-		res.end();
+  if (path === '/') {
+    res.writeHead(302, { 'Location': '/index.html' });
+    res.end();
 
-		return console.log(`Request [302]: ${path}`);
-	} else if (path === '/favicon.ico') {
-		res.writeHead(200, { 'Content-Type': 'text/plain' });
-		res.end('Favicon is unavailable.');
+    return console.log(`Request [302]: ${path}`);
+  } else if (path === '/favicon.ico') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Favicon is unavailable.');
 
-		return console.log(`Request [200]: ${path}`);
-	}
+    return console.log(`Request [200]: ${path}`);
+  }
 
-	const file = `dist${path}`;
-	const fileType = path.split('.').pop();
+  const file = `dist${path}`;
+  const fileType = path.split('.').pop();
 
-	fs.readFile(file, 'utf-8', (err, data) => {
-		if (!data) {
-			res.writeHead(404, { 'Content-Type': 'text/plain' });
-			res.end('Something went wrong. Please check the log.');
+  fs.readFile(file, 'utf-8', (err, data) => {
+    if (!data) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Something went wrong. Please check the log.');
 
-			return console.log(`Request [404]: ${path}\n${err}`);
-		}
+      return console.log(`Request [404]: ${path}\n${err}`);
+    }
 
-		res.writeHead(200, { 'Content-Type': `text/${fileType}` });
-		res.write(data);
-		res.end();
+    res.writeHead(200, { 'Content-Type': `text/${fileType}` });
+    res.write(data);
+    res.end();
 
-		console.log(`Request [200]: ${path}`);
-	});
+    console.log(`Request [200]: ${path}`);
+  });
 });
 
 server.listen(80, '0.0.0.0', () => {
-	console.log('Server running at http://localhost:3000/');
+  console.log('Server running at http://localhost:3000/');
 });
